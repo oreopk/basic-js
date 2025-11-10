4b44b0c88dbca406e51f010a3ee464634fae8ff2
oreopk <sart6zaV@yandex.ru>
2025-11-11 00:21:38 +0300

    feat: done encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  if(!str || str.length ===0){
+    return '';
+  }
+  let encoderLine = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      if (count > 1) {
+        encoderLine += count + str[i];
+      } else {
+        encoderLine += str[i];
+      }
+      count = 1;
+    }
+  }
+  return encoderLine;
 }
 
 module.exports = {
